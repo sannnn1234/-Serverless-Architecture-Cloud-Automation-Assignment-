37bde321c12dfbeeb083b2182a8f93b453e9abbf
sannnn1234 <guptasandeepkumar509@gmail.com>
2026-03-24 00:33:10 +0530

    lambda_function.py file

---
+++ Assigment1/lambda_function.py
@@ -0,0 +1,50 @@
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def get_instances_by_tag(ec2_client, tag_value):
+    """Return list of instance IDs matching Action=<tag_value>."""
+    response = ec2_client.describe_instances(
+        Filters=[
+            {'Name': 'tag:Action', 'Values': [tag_value]},
+            {'Name': 'instance-state-name',
+             'Values': ['running'] if tag_value == 'Auto-Stop' else ['stopped']}
+        ]
+    )
+    instance_ids = [
+        instance['InstanceId']
+        for reservation in response['Reservations']
+        for instance in reservation['Instances']
+    ]
+    return instance_ids
+
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name='ap-south-1')  # change region as needed
+
+    # Stop instances tagged Auto-Stop
+    stop_ids = get_instances_by_tag(ec2, 'Auto-Stop')
+    if stop_ids:
+        ec2.stop_instances(InstanceIds=stop_ids)
+        logger.info(f"Stopped instances: {stop_ids}")
+    else:
+        logger.info("No running instances found with tag Auto-Stop")
+
+    # Start instances tagged Auto-Start
+    start_ids = get_instances_by_tag(ec2, 'Auto-Start')
+    if start_ids:
+        ec2.start_instances(InstanceIds=start_ids)
+        logger.info(f"Started instances: {start_ids}")
+    else:
+        logger.info("No stopped instances found with tag Auto-Start")
+
+    return {
+        'statusCode': 200,
+        'body': {
+            'stopped': stop_ids,
+            'started': start_ids
+        }
+    }
